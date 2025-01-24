07b25f6eb30345ee02f8fb11f559e08e744638ac
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2025-01-24 14:03:36 +0700

    Added theme handler

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const HomePage = async () => {
+  // await delay(2000)
+  return <Button>Prostore</Button>
+}
+
+export default HomePage
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-const HomePage = () => {
-  return <Button>Prostore</Button>
-}
-
-export default HomePage
