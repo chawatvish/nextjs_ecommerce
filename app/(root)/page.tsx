0343017dbf99f23dcd024cd5ca5e6e3fb21b9b82
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2025-01-25 15:53:04 +0700

    finish draft home page with some mock products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from '@/components/ui/button'
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
 
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const HomePage = async () => {
   // await delay(2000)
-  return <Button>Prostore</Button>
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  )
 }
 
 export default HomePage
